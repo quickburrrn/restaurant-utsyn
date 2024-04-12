33c81f1b1280281b4e4cf925ffaa1b53d8cdfb59
Andre Hansen <andre.hansen146@gmail.com>
2024-04-12 10:10:51 +0200

    hello

---
+++ src/components/InfoField.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InfoField = (props) => {
+    return(
+        <div className="">
+            <h1>{children}</h1>
+        </div>
+    );
+}
+
+export default Alert
--- src/pages/Kvitering.js
+++ src/pages/Kvitering.js
@@ -1,15 +1,18 @@
 import { Link, useOutletContext } from "react-router-dom";
 import { useState } from "react";
+//import InfoField from "../components/InfoField";
 
 function Kvitering()
 {
 
     const [count, setCount] = useOutletContext()[0];
 
+
     return(
         <div>
             <h1>Reservasjonen er sendt</h1>
             <h3>{"Dato " + count}</h3>
+            {/* <InfoField>Hello</InfoField> */}
         </div>
     );
 }
