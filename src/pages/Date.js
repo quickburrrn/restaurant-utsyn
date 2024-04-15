c1d7f6180da133cad8a346730b768363d6c43df4
Andre <andre.hansen146@gmail.com>
2024-04-15 10:19:21 +0100

    redesignet dato selektor

--- src/pages/Date.js
+++ src/pages/Date.js
@@ -4,7 +4,7 @@ import TestButton from "../components/TestButton";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
 import { useState } from "react";
-
+import './styles.css'
 function Date()
 {
     const [count, setCount] = useOutletContext()[0];
@@ -19,7 +19,7 @@ function Date()
     };
     return (
         <div className="container text-center">
-            <div className="col mb-2" style={{display: "grid", placeItems: "center"}}>
+            {/* <div className="col mb-2" style={{display: "grid", placeItems: "center"}}>
                 <Calendar className="primary" onChange={setDay}/>
             </div>
 
@@ -32,7 +32,31 @@ function Date()
                 <Link to={`../tid`}><TestButton color="primary" >Veld Tidpunk</TestButton></Link>
                  : 
                 <TestButton color="secondary disabled">Veld Tidpunk</TestButton>}
-            </div>
+            </div> */}
+
+            <ul className="list-group list-group-flush">
+                {[...Array(11).keys()].map((item, index) => (
+                    <li className="list-group-item">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h1 className="display-2"><b>{index}</b></h1>
+                                <h4 className="">Tirsdag</h4>
+                            </div>
+                            <div class="col-8">
+                                <h1 className="ps-5">Reserver Bord Denne Dagen</h1>
+                            </div>
+                            <div class="col">
+
+                            </div>
+                            
+                        </div>
+                        <div>
+                            hei
+                        </div>
+                           
+                    </li>
+                ))}
+            </ul>
 
         </div>
     );
